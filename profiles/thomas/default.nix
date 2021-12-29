1d4f5fe401379064b392ecf404db34fb18d88a53
Thomas Boerger <thomas@webhippie.de>
2021-12-29 12:55:11 +0100

    fix: comment out file for now as it is wrong

--- profiles/thomas/default.nix
+++ profiles/thomas/default.nix
@@ -65,60 +65,60 @@
       polybar = import ./services/polybar.nix { inherit pkgs; };
     };
 
-    file = {
-      ".local/bin/git-amend" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-amend);
-      };
-      ".local/bin/git-conflicts" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-conflicts);
-      };
-      ".local/bin/git-gh-pages" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-gh-pages);
-      };
-      ".local/bin/git-promote" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-promote);
-      };
-      ".local/bin/git-recently-checkout-branches" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-recently-checkout-branches);
-      };
-      ".local/bin/git-resolve" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-resolve);
-      };
-      ".local/bin/git-unpushed-branches" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-unpushed-branches);
-      };
-      ".local/bin/git-unreleased" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-unreleased);
-      };
-      ".local/bin/git-where" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/git-where);
-      };
-      ".local/bin/search-and-replace" = {
-        executable = true;
-        text = (builtins.readFile ./scripts/search-and-replace);
-      };
+    # file = {
+    #   ".local/bin/git-amend" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-amend);
+    #   };
+    #   ".local/bin/git-conflicts" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-conflicts);
+    #   };
+    #   ".local/bin/git-gh-pages" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-gh-pages);
+    #   };
+    #   ".local/bin/git-promote" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-promote);
+    #   };
+    #   ".local/bin/git-recently-checkout-branches" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-recently-checkout-branches);
+    #   };
+    #   ".local/bin/git-resolve" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-resolve);
+    #   };
+    #   ".local/bin/git-unpushed-branches" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-unpushed-branches);
+    #   };
+    #   ".local/bin/git-unreleased" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-unreleased);
+    #   };
+    #   ".local/bin/git-where" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/git-where);
+    #   };
+    #   ".local/bin/search-and-replace" = {
+    #     executable = true;
+    #     text = (builtins.readFile ./scripts/search-and-replace);
+    #   };
 
-      # ".local/wallpapers/dark.jpg" = {
-      #   executable = false;
-      #   text = (builtins.readFile ./wallpapers/dark.jpg);
-      # };
-      # ".local/wallpapers/light.jpg" = {
-      #   executable = false;
-      #   text = (builtins.readFile ./wallpapers/light.jpg);
-      # };
-      # ".local/wallpapers/tower.jpg" = {
-      #   executable = false;
-      #   text = (builtins.readFile ./wallpapers/tower.jpg);
-      # };
-    };
+    #   ".local/wallpapers/dark.jpg" = {
+    #     executable = false;
+    #     text = (builtins.readFile ./wallpapers/dark.jpg);
+    #   };
+    #   ".local/wallpapers/light.jpg" = {
+    #     executable = false;
+    #     text = (builtins.readFile ./wallpapers/light.jpg);
+    #   };
+    #   ".local/wallpapers/tower.jpg" = {
+    #     executable = false;
+    #     text = (builtins.readFile ./wallpapers/tower.jpg);
+    #   };
+    # };
   };
 }
