c8edc0054e927c89abe01b20d1eca07892637830
Thomas Boerger <thomas@webhippie.de>
2022-03-21 10:09:23 +0100

    programs: add more hosts to ssh config

--- profiles/thomas/programs/ssh.nix
+++ profiles/thomas/programs/ssh.nix
@@ -4,6 +4,20 @@
   enable = true;
 
   matchBlocks = {
+    "*.cloudpunks.io !jumphost*.cloudpunks.io" = {
+      user = "oper";
+      forwardAgent = true;
+      proxyJump = "tboerger@jumphost1.cloudpunks.io";
+    };
+    "jumphost1.cloudpunks.io" = {
+      user = "tboerger";
+      forwardAgent = true;
+    };
+    "jumphost2.cloudpunks.io" = {
+      user = "tboerger";
+      forwardAgent = true;
+    };
+
     "bous" = {
       hostname = "bous.kleister.tech";
       port = 22022;
@@ -75,24 +89,75 @@
     "*.webhippie.de !bastion.webhippie.de" = {
       user = "thomas";
       forwardAgent = true;
+      proxyJump = "thomas@bastion.webhippie.de";
     };
     "bastion.webhippie.de" = {
       user = "thomas";
       forwardAgent = true;
-      proxyJump = "thomas@bastion.webhippie.de";
     };
 
-    "*.cloudpunks.io !jumphost*.cloudpunks.io" = {
-      user = "oper";
+    "*.easy01.proactcloud.de !bastion-01.easy01.proactcloud.de" = {
+      user = "detbo";
       forwardAgent = true;
-      proxyJump = "tboerger@jumphost1.cloudpunks.io";
+      proxyJump = "detbo@bastion-01.easy01.proactcloud.de";
     };
-    "jumphost1.cloudpunks.io" = {
-      user = "tboerger";
+    "bastion-01.easy01.proactcloud.de" = {
+      user = "detbo";
       forwardAgent = true;
     };
-    "jumphost2.cloudpunks.io" = {
-      user = "tboerger";
+
+    "*.easy02.proactcloud.de !bastion-01.easy02.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+      proxyJump = "detbo@bastion-01.easy02.proactcloud.de";
+    };
+    "bastion-01.easy02.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+    };
+
+    "*.ins01.proactcloud.de !bastion-01.ins01.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+      proxyJump = "detbo@bastion-01.ins01.proactcloud.de";
+    };
+    "bastion-01.ins01.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+    };
+
+    "*.chef01.proactcloud.de !bastion-01.chef01.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+      proxyJump = "detbo@bastion-01.chef01.proactcloud.de";
+    };
+    "bastion-01.chef01.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+    };
+
+    "*.sch01.proactcloud.de !bastion-01.sch01.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+      proxyJump = "detbo@bastion-01.sch01.proactcloud.de";
+    };
+    "bastion-01.sch01.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+    };
+
+    "*.internal.proactcloud.de !bastion-01.internal.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+      proxyJump = "detbo@bastion-01.internal.proactcloud.de";
+    };
+    "bastion-01.internal.proactcloud.de" = {
+      user = "detbo";
+      forwardAgent = true;
+    };
+
+    "mothership.proactcloud.de" = {
+      user = "detbo";
       forwardAgent = true;
     };
   };
