e46bfe05f34d95b8547276caf54e89644f0625e4
Thomas Boerger <thomas@webhippie.de>
2022-03-21 09:55:59 +0100

    overlays: update dockutil and drop old stuff

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,6 @@
 self: super:
 
 {
-  # anyconfig = super.callPackage ./anyconfig {};
-  # ansible-later = super.callPackage ./ansible-later {};
   dockutil = super.callPackage ./dockutil { };
   mysides = super.callPackage ./mysides { };
 }
--- overlays/dockutil/default.nix
+++ overlays/dockutil/default.nix
@@ -1,33 +1,32 @@
-{ lib, stdenv, fetchurl, libarchive, p7zip }:
-with lib;
+{ lib, stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "dockutil";
   version = "2.0.5";
 
-  src = fetchurl {
-    url = "https://github.com/kcrawford/dockutil/releases/download/${version}/dockutil-${version}.pkg";
-    sha256 = "sha256-kZ7dOG8SSh25DgcbvkgPvEFGZIVz4fv0kWN41pbeNlw=";
+  src = fetchFromGitHub {
+    owner  = "kcrawford";
+    repo   = "dockutil";
+    rev    = version;
+    sha256 = "sha256-8tDkueCTCtvxc7owp3K9Tsrn4hL79CM04zBNv7AcHgA=";
   };
 
   dontBuild = true;
-  nativeBuildInputs = [ libarchive p7zip ];
-
-  unpackPhase = ''
-    7z x $src
-    bsdtar -xf Payload~
-  '';
 
   installPhase = ''
-    mkdir -pv $out/bin
-    cp "usr/local/bin/dockutil" "$out/bin/dockutil"
+    runHook preInstall
+
+    mkdir -p $out/bin
+    install -Dm755 scripts/dockutil -t $out/bin
+
+    runHook postInstall
   '';
 
-  meta = {
+  meta = with lib; {
     description = "Tool for managing dock items";
     homepage = "https://github.com/kcrawford/dockutil";
     license = licenses.asl20;
-    maintainers = [ "tboerger" ];
+    maintainers = with maintainers; [ tboerger ];
     platforms = platforms.darwin;
   };
 }
