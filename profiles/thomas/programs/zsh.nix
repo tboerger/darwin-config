bfa3a45f25ee72806ca6e1a12f491018119780ce
Thomas Boerger <thomas@webhippie.de>
2024-08-31 22:21:15 +0200

    fix: drop wrong home-manager usage from zsh config

--- profiles/thomas/programs/zsh.nix
+++ profiles/thomas/programs/zsh.nix
@@ -19,64 +19,62 @@ in
   };
 
   config = mkIf cfg.enable {
-    home-manager.users."${config.profile.username}" = { config, ... }: {
-      programs = {
-        zsh = {
+    programs = {
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+
+        autosuggestion = {
           enable = true;
-          enableCompletion = true;
+        };
 
-          autosuggestion = {
-            enable = true;
-          };
+        syntaxHighlighting = {
+          enable = true;
+        };
 
-          syntaxHighlighting = {
-            enable = true;
-          };
+        profileExtra = ''
+          [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
+        '';
 
-          profileExtra = ''
-            [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
-          '';
+        history = {
+          size = 10000000;
+          save = 10000000;
+          extended = true;
+          path = "${config.home.homeDirectory}/.zhistory";
+        };
 
-          history = {
-            size = 10000000;
-            save = 10000000;
-            extended = true;
-            path = "${config.home.homeDirectory}/.zhistory";
-          };
+        sessionVariables = {
+          EDITOR = "vim";
+          PAGER = "less";
+          CLICOLOR = "1";
+          GREP_COLOR = "mt=1;33";
+          IGNOREEOF = "1";
+        };
 
-          sessionVariables = {
-            EDITOR = "vim";
-            PAGER = "less";
-            CLICOLOR = "1";
-            GREP_COLOR = "mt=1;33";
-            IGNOREEOF = "1";
-          };
+        shellAliases = {
+          ".." = "cd ..";
+          "..." = "cd ../..";
 
-          shellAliases = {
-            ".." = "cd ..";
-            "..." = "cd ../..";
+          rgrep = "grep -Rn";
+          hgrep = "fc -El 0 | grep";
+          history = "fc -l 1";
+          sha256sum = "shasum -a 256";
+        };
 
-            rgrep = "grep -Rn";
-            hgrep = "fc -El 0 | grep";
-            history = "fc -l 1";
-            sha256sum = "shasum -a 256";
-          };
+        oh-my-zsh = {
+          enable = true;
 
-          oh-my-zsh = {
-            enable = true;
-
-            plugins = [
-              "direnv"
-              "encode64"
-              "git-prompt"
-              "history-substring-search"
-              "kube-ps1"
-              "rsync"
-              "sudo"
-              "tmux"
-              "transfer"
-            ];
-          };
+          plugins = [
+            "direnv"
+            "encode64"
+            "git-prompt"
+            "history-substring-search"
+            "kube-ps1"
+            "rsync"
+            "sudo"
+            "tmux"
+            "transfer"
+          ];
         };
       };
     };
