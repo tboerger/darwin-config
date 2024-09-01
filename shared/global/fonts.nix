d9a720219e2c24e7518b03f76198096ab0278490
Thomas Boerger <thomas@webhippie.de>
2024-09-01 12:39:03 +0200

    fix: drop fonts for now for darwin compat

--- shared/global/fonts.nix
+++ shared/global/fonts.nix
@@ -5,15 +5,15 @@ with lib;
   config = {
     fonts = {
       packages = with pkgs; [
-        corefonts
-        fira-code-nerdfont
-        font-awesome
-        nerdfonts
-        noto-fonts
-        noto-fonts-emoji
-        noto-fonts-extra
-        roboto
-        source-code-pro
+        # corefonts
+        # fira-code-nerdfont
+        # font-awesome
+        # nerdfonts
+        # noto-fonts
+        # noto-fonts-emoji
+        # noto-fonts-extra
+        # roboto
+        # source-code-pro
       ];
     };
   };
