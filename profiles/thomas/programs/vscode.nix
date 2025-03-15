9b45fa044d581d3d9274f5c2f06ad313d4ce2a38
Thomas Boerger <thomas@webhippie.de>
2025-03-15 17:14:24 +0100

    feat: move vscode config into new place

--- profiles/thomas/programs/vscode.nix
+++ profiles/thomas/programs/vscode.nix
@@ -28,72 +28,77 @@ in
     programs = {
       vscode = {
         enable = true;
-        enableUpdateCheck = false;
-        enableExtensionUpdateCheck = false;
         mutableExtensionsDir = false;
 
-        extensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          bradlc.vscode-tailwindcss
-          christian-kohler.npm-intellisense
-          dracula-theme.theme-dracula
-          dzhavat.bracket-pair-toggler
-          editorconfig.editorconfig
-          foxundermoon.shell-format
-          github.vscode-github-actions
-          golang.go
-          grafana.vscode-jsonnet
-          Grafana.vscode-jsonnet
-          hashicorp.terraform
-          jnoortheen.nix-ide
-          mads-hartmann.bash-ide-vscode
-          mikestead.dotenv
-          ms-azuretools.vscode-docker
-          ms-kubernetes-tools.vscode-kubernetes-tools
-          ms-python.python
-          ms-vscode-remote.remote-containers
-          ms-vscode-remote.remote-ssh
-          ms-vscode.makefile-tools
-          naumovs.color-highlight
-          redhat.vscode-yaml
-          shakram02.bash-beautify
-          signageos.signageos-vscode-sops
-          steoates.autoimport
-          timonwong.shellcheck
-          Vue.volar
-          yzhang.markdown-all-in-one
-          zxh404.vscode-proto3
-        ];
+        profiles = {
+          default = {
+            enableUpdateCheck = false;
+            enableExtensionUpdateCheck = false;
 
-        userSettings = {
-          "workbench.colorTheme" = "Dracula";
-          "workbench.startupEditor" = "none";
+            extensions = with pkgs.vscode-extensions; [
+              bbenoist.nix
+              bradlc.vscode-tailwindcss
+              christian-kohler.npm-intellisense
+              dracula-theme.theme-dracula
+              dzhavat.bracket-pair-toggler
+              editorconfig.editorconfig
+              foxundermoon.shell-format
+              github.vscode-github-actions
+              golang.go
+              grafana.vscode-jsonnet
+              Grafana.vscode-jsonnet
+              hashicorp.terraform
+              jnoortheen.nix-ide
+              mads-hartmann.bash-ide-vscode
+              mikestead.dotenv
+              ms-azuretools.vscode-docker
+              ms-kubernetes-tools.vscode-kubernetes-tools
+              ms-python.python
+              ms-vscode-remote.remote-containers
+              ms-vscode-remote.remote-ssh
+              ms-vscode.makefile-tools
+              naumovs.color-highlight
+              redhat.vscode-yaml
+              shakram02.bash-beautify
+              signageos.signageos-vscode-sops
+              steoates.autoimport
+              timonwong.shellcheck
+              Vue.volar
+              yzhang.markdown-all-in-one
+              zxh404.vscode-proto3
+            ];
 
-          "redhat.telemetry.enabled" = true;
-          "git.enableSmartCommit" = true;
+            userSettings = {
+              "workbench.colorTheme" = "Dracula";
+              "workbench.startupEditor" = "none";
 
-          "editor.renderControlCharacters" = true;
-          "editor.renderWhitespace" = "all";
-          "editor.minimap.enabled" = false;
+              "redhat.telemetry.enabled" = true;
+              "git.enableSmartCommit" = true;
 
-          "editor.rulers" = [
-            80
-          ];
+              "editor.renderControlCharacters" = true;
+              "editor.renderWhitespace" = "all";
+              "editor.minimap.enabled" = false;
 
-          "files.insertFinalNewline" = true;
-          "files.trimTrailingWhitespace" = true;
+              "editor.rulers" = [
+                80
+              ];
 
-          "go.useLanguageServer" = true;
-          "go.toolsManagement.autoUpdate" = true;
+              "files.insertFinalNewline" = true;
+              "files.trimTrailingWhitespace" = true;
 
-          "[python]" = {
-            "editor.formatOnType" = true;
-          };
-          "[vue]" = {
-            "editor.defaultFormatter" = "Vue.volar";
-          };
-          "[yaml]" = {
-            "editor.defaultFormatter" = "redhat.vscode-yaml";
+              "go.useLanguageServer" = true;
+              "go.toolsManagement.autoUpdate" = true;
+
+              "[python]" = {
+                "editor.formatOnType" = true;
+              };
+              "[vue]" = {
+                "editor.defaultFormatter" = "Vue.volar";
+              };
+              "[yaml]" = {
+                "editor.defaultFormatter" = "redhat.vscode-yaml";
+              };
+            };
           };
         };
       };
